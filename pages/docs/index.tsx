b801c9f05c0c1ff0127ba2a817940cbcd00e235f
Benjamin Chiu <894306909@qq.com>
2021-12-11 23:37:55 +0800

    api Props

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-10 20:21:15
- * @LastEditTime: 2021-12-11 23:33:41
+ * @LastEditTime: 2021-12-11 23:37:35
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \console-next\pages\about\index.tsx
@@ -10,7 +10,10 @@
 import type { NextPage } from "next";
 import styles from "../../styles/Home.module.css";
 import Layout from "@/components/layout";
-const Docs: NextPage = ({ stars }) => {
+interface DocsProps {
+  stars: number
+}
+const Docs: NextPage<DocsProps> = ({ stars }) => {
   return (
     <>
       <h1 className={styles.title}>这是一个文档</h1>
