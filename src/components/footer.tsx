c0722d619a7363bb8709da2a56b34bd8dd6ec314
Benjamin Chiu <894306909@qq.com>
2021-12-12 15:00:33 +0800

    修改产品中心

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-10 21:21:52
- * @LastEditTime: 2021-12-12 14:21:49
+ * @LastEditTime: 2021-12-12 14:34:27
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \console-next\components\footer.tsx
@@ -10,24 +10,24 @@ import styles from "./compnent.module.less";
 import Link from 'next/link'
 
 const links = [{
-  category: 'General resources',
-  links: ['Docs','Learn','Showcase','Blog', 'Analytics','Next.js Conf', 'Live' ]
+  category: '资源',
+  links: ['文档','学习','案例','博客', '分析','配置', '直播' ]
 },{
-  category: 'More',
-  links: ['Commerce','Contact', 'Releases' ]
+  category: '更多',
+  links: ['商务','联系', 'Releases' ]
 },{
-  category: 'About Benjamin',
+  category: '关于',
   links: ['Github','Open Source Software' ]
 },{
-  category: 'Legal',
-  links: ['Privacy Policy' ]
+  category: '法律',
+  links: ['隐私政策' ]
 }]
 const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.links}>
       {
         links.map(item => {
-          return <div key={`${item.category} + o`}>
+          return <div key={`${item.category} + o`} className={styles.category}>
           <h4 key={item.category}>{item.category}</h4>
           <div className={styles.link}>
             {
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,24 +1,81 @@
 /*
  * @Author: your name
  * @Date: 2021-12-10 20:21:15
- * @LastEditTime: 2021-12-10 21:04:56
+ * @LastEditTime: 2021-12-12 14:59:48
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \console-next\pages\about\index.tsx
  */
-
+import { Avatar, Card, Col, Row } from "antd";
 import type { NextPage } from "next";
-import styles from "../../styles/Home.module.css"; 
+import styles from "../../styles/Home.module.css";
 import { Fragment } from "react";
 import Head from "next/head";
+const { Meta } = Card;
 const Products: NextPage = () => {
   return (
     <Fragment>
       <Head>
         <title>Popochiu - 产品中心</title>
-        <meta name="description" content="popochiu" /> 
+        <meta name="description" content="popochiu" />
       </Head>
       <h1 className={styles.title}>产品中心</h1>
+
+      <div className="site-card-wrapper">
+        <Row >
+          <Col xs={20} sm={16} md={12} lg={8} xl={4}>
+            <Card
+              style={{ width: 300,  marginBottom: 24 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+            >
+              <Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title="Card title"
+                description="This is the description"
+              />
+            </Card>
+          </Col> 
+          <Col xs={20} sm={16} md={12} lg={8} xl={4}>
+            <Card
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+            >
+              <Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title="Card title"
+                description="This is the description"
+              />
+            </Card> 
+          </Col> 
+          <Col xs={20} sm={16} md={12} lg={8} xl={4}>
+            <Card
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+            >
+              <Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title="Card title"
+                description="This is the description"
+              />
+            </Card> 
+          </Col>
+        </Row>
+      </div>
     </Fragment>
   );
 };
